2381be5b602fe57fae9a4807219f8de989d884fb
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-07 12:23:49 +0100

    Add single-quote rule to editor config and refresh files

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -1,9 +1,5 @@
-import gentelmanData from "../../gentelman.data";
-
 const Info = () => {
-  const count = gentelmanData.map((gentleman) => gentleman.selected);
-
-  return <p className="info">{count}0 gentlemen pointing at you</p>;
+  return <p className="info"> gentlemen pointing at you</p>;
 };
 
 export default Info;
